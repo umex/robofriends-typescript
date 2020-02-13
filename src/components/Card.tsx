fbd37500550797e5e7fbd50d7a3c5dd993b18ea9
bomko <dejan.obrez@gmail.com>
2020-02-13 23:50:49 +0100

    typescript

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 
-const Card = ({ name, email, id }) => {
+interface CardStatelessProps {
+  name: string,
+  email: string,
+  id: number
+}
+
+//sfc se nahaja v node_modules @types, react index.d.ts
+const Card: React.SFC<CardStatelessProps> = ({ name, email, id }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
       <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
--- src/components/Scroll.tsx
+++ src/components/Scroll.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
-const Scroll = (props) => {
+type Props = {
+  //jsx po domace je element javascriptu v reactu ki zgleda kot html -> poglej scroll v App.tsx
+  children?: JSX.Element
+}
+const Scroll = (props:Props) => {
   return (
     <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
       {props.children}
